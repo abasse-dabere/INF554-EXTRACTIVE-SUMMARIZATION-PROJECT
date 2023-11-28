c179adce94a25819bc8426fcadd4866402b81c4f
rovanaud <76216339+rovanaud@users.noreply.github.com>
2023-11-28 15:45:31 +0100

    [+]  defining pre-processing rules

---
+++ processing.py
@@ -0,0 +1,37 @@
+import re
+import unicodedata
+import nltk
+# If not downloaded
+# nltk.download('stopwords')
+# nltk.download('punkt')
+from nltk.corpus import stopwords
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+stop_words = stopwords.words('english')
+
+# Converts the unicode file to ascii
+def unicode_to_ascii(s):
+    return ''.join(c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn')
+
+def preprocess_sentence(w):
+    w = unicode_to_ascii(w.lower().strip())
+    # creating a space between a word and the punctuation following it
+    # eg: "he is a boy." => "he is a boy ."
+    w = re.sub(r"([?.!,¿])", r" \1 ", w)
+    w = re.sub(r'[" "]+', " ", w)
+    # replacing everything with space except (a-z, A-Z, ".", "?", "!", ",")
+    w = re.sub(r"[^a-zA-Z?.!]+", " ", w)
+    w = re.sub(r'\b\w{0,2}\b', '', w)
+
+    #remove vocalsound
+    w = w.replace("vocalsound", '')
+
+    # remove stopword
+    mots = word_tokenize(w.strip())
+    mots = [mot for mot in mots if mot not in stop_words]
+    return ' '.join(mots).strip()
+
+def process(X_training_original):
+  return np.array([preprocess_sentence(x) for x in X_training_original])
